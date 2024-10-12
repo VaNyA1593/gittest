a38ce70586d9204439b9c3e1ec2529641e60b708
VaNyA1593 <vankaboiko1593@gmail.com>
2024-10-12 18:40:43 +0100

    Makes a smiley face!

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from turtle import *
+
 print("Hello, first commit!")
 print("Second commit!")
+
+pensize(10)
+
+right(90)
+
+for i in range(90):
+    forward(2)
+    left(2)
+
+penup()
+goto(40,40)
+pendown()
+forward(10)
+
+penup()
+goto(80,40)
+pendown()
+forward(10)
+
+exitonclick()
